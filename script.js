edfa79b87999b32bbd1e6ae5ae84a9ca32abb358
Johnny Braga <johnnybraga2@gmail.com>
2024-09-02 14:30:01 -0300

    alteração no card

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ document.getElementById('loginForm').addEventListener('submit', function(event) 
     event.preventDefault();
     document.querySelector('.card-inner').classList.add('card-flip');
 });
+
+document.getElementById('backButton').addEventListener('click', function() {
+    document.querySelector('.card-inner').classList.remove('card-flip');
+});
